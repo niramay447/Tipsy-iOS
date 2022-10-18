c0de6fc2818ab503f0c3e6e5436b19707630ab90
niramay447 <nk447@snu.edu.in>
2022-10-18 11:54:12 +0530

    working stepper and labels

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -11,12 +11,26 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPcButton: UIButton!
     @IBOutlet weak var zeroPctbutton: UIButton!
 
-
-    @IBAction func tipChanged(_ sender: Any) {
+    var tip = 0.10
+    var people = 2
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctbutton.isSelected = false
+        tenPcButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        tip = (Double(buttonTitle.dropLast())!) / 100
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        people = Int(sender.value)
+        
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
     }
     
 }
